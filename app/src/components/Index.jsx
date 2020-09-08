22271b100dd435b04d0ffae2110412d9610aa37e
Ajay051198 <ajay.uom@gmail.com>
2020-09-08 19:00:03 +0530

    init master deploy

---
+++ app/src/components/Index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./index.sass"
+
+function Index() {
+    return (
+        <main className="index">
+            Im working on it ! :O
+        </main>
+    )
+}
+
+export default Index
